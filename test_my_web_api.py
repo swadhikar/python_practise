02f647e3612bef1c9400cd876add254d745a6250
Trainer Name <trainer@example.com>
2024-08-29 17:34:29 +0530

    Added webapi changes with test cases

--- test_my_web_api.py
+++ test_my_web_api.py
@@ -1,23 +1,27 @@
-# import requests
-# import json
+import requests
+import json
 
 
-# def test_read_root():
-#     url: str = "http://localhost:8000/"
-#     response = requests.get(url)
-#     return json.loads(response.content.decode())
+def test_read_root():
+    url: str = "http://localhost:8000/"
+    response = requests.get(url)
+    assert json.loads(response.content.decode()) == {
+                                                        "hello": "Hello world!",
+                                                        "user": {
+                                                            "name": "swadhikar",
+                                                            "company": "TEL",
+                                                            "age": 34
+                                                        }
+                                                    }
 
+def test_read_item():
+    url: str = f'http://localhost:8000/items/user'
+    response = requests.get(url)
+    return json.loads(response.content.decode()) == {
+                                                        "name": "swadhikar",
+                                                        "company": "TEL",
+                                                        "age": 34
+                                                    }
 
-# def test_read_item(endpoint: str):
-#     url: str = f'http://localhost:8000/{endpoint}'
-#     response = requests.get(url)
-#     return json.loads(response.content.decode())
-
-
-# if __name__ == '__main__':
-#     return_dict = test_read_root()
-#     print("Get full response:\n", json.dumps(return_dict, indent=4))
-#     return_dict = test_read_item(r'items/user')
-#     print("Get items/user response:\n", json.dumps(return_dict, indent=4))
     
 
